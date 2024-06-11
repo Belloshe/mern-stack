c1f33c32462f00e8361ff27531ffb82928e77a1c
Belloshe <isabelle.ibrahimsson@edu.cmeducations.se>
2024-06-11 21:04:59 +0200

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = process.env.MONGODB_URI;  
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
