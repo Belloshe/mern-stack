3b2e20d9e180ed889d6e4321e9c0a2ad47f507a2
Belloshe <isabelle.ibrahimsson@edu.cmeducations.se>
2024-06-13 00:43:17 +0200

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -8,10 +8,11 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true
     });
     console.log('MongoDB connected');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
