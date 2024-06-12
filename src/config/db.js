34bf2e8a3e9eaabd447a9c9c9ea78acca776d4cc
Belloshe <isabelle.ibrahimsson@edu.cmeducations.se>
2024-06-13 00:22:38 +0200

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
     });
     console.log('MongoDB connected');
   } catch (error) {
