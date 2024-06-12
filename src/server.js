d23f3790f757524496a10d7992069b51e20c669c
Belloshe <isabelle.ibrahimsson@edu.cmeducations.se>
2024-06-12 21:15:22 +0200

    changes

--- src/server.js
+++ src/server.js
@@ -4,29 +4,34 @@ const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
 const commentRoutes = require('./routes/comments');
 const dotenv = require('dotenv');
-const cors = require('cors'); 
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 
-
 connectDB();
-
 
 app.use(express.json());
 
+const allowedOrigins = ['http://localhost:3000', 'https://your-netlify-app.netlify.app'];
 
 app.use(cors({
-  origin: 'https://mern-stackk.netlify.app'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'API is working' });
 });
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
